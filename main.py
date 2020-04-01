4403e786009fc89a14bcd690acce978b3b813dc1
MatthewXcv <39726487+MatthewXcv@users.noreply.github.com>
2020-04-01 10:18:10 +0200

    ok
    ok

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import StringProperty, NumericProperty, ObjectProperty
 from kivy.core.window import Window
+from kivy.uix.label import Label
+
 class TheScreenManager(ScreenManager):
     pass
+
 class MainScreen(Screen):
-    def Exit(self):
-        quit()
+    def Something(self):
+        pass
+
 class default(App):
     def build(self):
+
+
+
         Window.clearcolor = 1,1,1,1
         return TheScreenManager()
 default().run()
